fb90993142042fff680c546e7b1730a2fa932ebf
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-06-13 17:39:41 +0800

    refractor

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,6 @@
 package iteration
 
-const count = 5
-
-func repeat(char string) (newChar string) {
+func repeat(char string, count int) (newChar string) {
 	for i := 0; i < count; i++ {
 		newChar += char
 	}
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,9 +3,15 @@ package iteration
 import "testing"
 
 func TestIterations(t *testing.T) {
-	got := repeat("a")
+	got := repeat("a", 5)
 	want := "aaaaa"
 	if got != want {
 		t.Errorf("got '%s' want s%s'", got, want)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		repeat("a", 5)
 	}
 }
