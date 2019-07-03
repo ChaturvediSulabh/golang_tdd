5c4a0eeb6788da43c84989e6250fb151c1ef2d67
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-07-03 16:57:39 +0800

    refractor

--- arrays_slices/sum/sum.go
+++ arrays_slices/sum/sum.go
@@ -7,3 +7,21 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, nums := range numbersToSum {
+		sums = append(sums, Sum(nums))
+	}
+	return
+}
+
+func SumAllTrails(numbersToSum ...[]int) (sums []int) {
+	for _, nums := range numbersToSum {
+		if len(nums) > 0 {
+			sums = append(sums, Sum(nums[1:]))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return
+}
--- arrays_slices/sum/sum_test.go
+++ arrays_slices/sum/sum_test.go
@@ -1,9 +1,13 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("Collection of numbers #1", func(t *testing.T) {
+
+	t.Run("Collection of whole numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
 		want := 6
@@ -11,5 +15,36 @@ func TestSum(t *testing.T) {
 		if want != got {
 			t.Errorf("got %d want %d, %v", got, want, numbers)
 		}
+	})
+
+	message := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
+	t.Run("Colllection of Slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		message(t, got, want)
+	})
+
+	t.Run("Collection of all tails of slices", func(t *testing.T) {
+		got := SumAllTrails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		message(t, got, want)
+	})
+
+	t.Run("Tail of an empty slice", func(t *testing.T) {
+		got := SumAllTrails([]int{}, []int{1, 4, 5})
+		want := []int{0, 9}
+		message(t, got, want)
+	})
+
+	t.Run("Tail of an empty slice", func(t *testing.T) {
+		got := SumAllTrails([]int{}, []int{1, 4, 5})
+		want := []int{0, 9}
+		message(t, got, want)
 	})
 }
